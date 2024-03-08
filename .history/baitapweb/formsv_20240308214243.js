b0efc63c5a80dff244419325ac5ba628b41f6ddc
G1tHub-zer0 <quockhang0605@gmail.com>
2024-03-08 22:03:30 +0700

    form

---
+++ .history/baitapweb/formsv_20240308214243.js
@@ -0,0 +1,22 @@
+const form = document.querySelector(
+  ".feedback-form"
+);
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const name =
+    document.getElementById("name").value;
+  const email =
+    document.getElementById("email").value;
+  const content =
+    document.getElementById("content").value;
+  const rating =
+    document.getElementById("rating").value;
+
+  // Gửi dữ liệu phản hồi đến server (chưa có code)
+
+  alert("Cảm ơn bạn đã phản hồi!");
+
+  form.reset();
+});
